c890818eb422e23e191ae83b1d09f114963ca78c
Natalie Brady <brady.visible@hotmail.com>
2018-10-04 13:01:20 +0100

    display fine even when returned

--- app/models/user_item.rb
+++ app/models/user_item.rb
@@ -10,16 +10,24 @@ class UserItem < ApplicationRecord
     def return_date
       (created_at + 2.day).strftime("%d/%m/%Y")
     end
+
+    def user_returned_date
+      (updated_at).strftime("%d/%m/%Y")
+    end
     
     # Fine for books that have been rented more than 2 days
     def fine
       date_of_return = Date.parse(return_date)
+      returned_date = Date.parse(user_returned_date)
       date_today = Date.today
       if date_today > date_of_return && returned == false
         fine = (date_today - date_of_return).to_i * 1.00
         return fine
-      else
-        return "0.00"
+      elsif date_today > date_of_return && returned == true
+        fine = (returned_date - date_of_return).to_i * 1.00
+        return fine
+      else 
+        return 0.00
       end
     end
 
