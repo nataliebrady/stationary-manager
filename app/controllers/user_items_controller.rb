11aa792eec9ed9ca4b8daa1478711089d246802b
Natalie Brady <brady.visible@hotmail.com>
2018-10-05 16:19:00 +0100

    add custom styling to stationary app

--- app/controllers/user_items_controller.rb
+++ app/controllers/user_items_controller.rb
@@ -1,4 +1,6 @@
 class UserItemsController < ApplicationController
+  before_action :logged_in_user, only: [:index, :destroy]
+  before_action :admin_user,     only: [:destroy]
 
   def index  
     # Find the current users user_items id and find those id's in the UserItem table 
@@ -36,6 +38,12 @@ class UserItemsController < ApplicationController
   	end
   end
 
+  def destroy 
+    UserItem.find(params[:id]).destroy
+    flash[:success] = "User history deleted"
+    redirect_to user_items_url
+  end
+
   # Returning action (sets the returned boolean to true and updates the book status to 'available')
   def returning
   	  @user_item = UserItem.find(params[:id])
@@ -48,6 +56,20 @@ class UserItemsController < ApplicationController
 
   def ordered_params
   	params.require(:user_item).permit(:item_id, :user_id)
+  end
+
+  # Confirms an admin user.
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
   end
 
 end
